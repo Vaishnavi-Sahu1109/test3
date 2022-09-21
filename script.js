556cbdecbe3f20ea6aa21d5a1f408ffdb12dc716
ShubhamVsCode <shubhamvscode@gmail.com>
2022-09-21 15:26:22 +0530

    This works

--- script.js
+++ script.js
@@ -1,140 +1,117 @@
-// Note: This example requires that you consent to location sharing when
-// prompted by your browser. If you see the error "The Geolocation service
-// failed.", it means you probably did not give permission for the browser to
-// locate you.
-let map, infoWindow;
+//javascript.js
+//set map options
 
-function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 22,
-  });
-  infoWindow = new google.maps.InfoWindow();
+const kmMsg = document.querySelector(".km");
 
-  const locationButton = document.createElement("button");
+var myLatLng = { lat: 38.346, lng: -0.4907 };
+var mapOptions = {
+  center: myLatLng,
+  zoom: 9,
+  mapTypeId: google.maps.MapTypeId.ROADMAP,
+};
 
-  locationButton.textContent = "Current Location";
-  locationButton.classList.add("custom-map-control-button");
-  map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
-  locationButton.addEventListener("click", () => {
-    // Try HTML5 geolocation.
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          const pos = {
-            lat: position.coords.latitude,
-            lng: position.coords.longitude,
-          };
+//create map
+var map = new google.maps.Map(document.getElementById("googleMap"), mapOptions);
 
-          infoWindow.setPosition(pos);
-          infoWindow.setContent("Location found.");
-          infoWindow.open(map);
-          map.setCenter(pos);
-        },
-        () => {
-          handleLocationError(true, infoWindow, map.getCenter());
-        }
-      );
+//create a DirectionsService object to use the route method and get a result for our request
+var directionsService = new google.maps.DirectionsService();
+
+//create a DirectionsRenderer object which we will use to display the route
+var directionsDisplay = new google.maps.DirectionsRenderer();
+
+//bind the DirectionsRenderer to the map
+directionsDisplay.setMap(map);
+
+//define calcRoute function
+function calcRoute() {
+  directionsDisplay.setMap(map);
+
+  //create request
+  var request = {
+    origin: document.getElementById("from").value,
+    destination: document.getElementById("to").value,
+    travelMode: google.maps.TravelMode.DRIVING, //WALKING, BYCYCLING, TRANSIT
+    unitSystem: google.maps.UnitSystem.METRIC,
+  };
+
+  //pass the request to the route method
+  directionsService.route(request, function (result, status) {
+    if (status == google.maps.DirectionsStatus.OK) {
+      //Get distance and time
+      const output = document.querySelector("#output");
+      output.innerHTML =
+        "<div> The distance between " +
+        document.getElementById("from").value.bold() +
+        " & " +
+        document.getElementById("to").value.bold() +
+        " is " +
+        result.routes[0].legs[0].distance.text.bold() +
+        "." +
+        "<br />Duration  : " +
+        result.routes[0].legs[0].duration.text.bold() +
+        ".</div>";
+      const manResult = result.routes[0].legs[0].distance.text;
+      kmMsg.innerHTML = manResult;
+
+      //display route
+      directionsDisplay.setDirections(result);
     } else {
-      // Browser doesn't support Geolocation
-      handleLocationError(false, infoWindow, map.getCenter());
+      //delete route from map
+      directionsDisplay.setDirections({ routes: [] });
+      //center map in London
+      map.setCenter(myLatLng);
+
+      //show error message
+      output.innerHTML =
+        "<div class='alert-danger'><i class='fas fa-exclamation-triangle'></i> Could not retrieve driving distance.</div>";
     }
   });
 }
 
-function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-  infoWindow.setPosition(pos);
-  infoWindow.setContent(
-    browserHasGeolocation
-      ? "Error: The Geolocation service failed."
-      : "Error: Your browser doesn't support geolocation."
-  );
-  infoWindow.open(map);
-}
+//create autocomplete objects for all inputs
+var options = {
+  types: ["(cities)"],
+};
 
-// let autocomplete;
-// function initAutocomplete() {
-//   autocomplete = new google.maps.Autocomplete(
-//     document.getElementById("autocomplete"),
-//     {
-//       componentRestrictions: { country: "us" },
-//       fields: ["address_components", "geometry", "icon", "name"],
-//       strictBounds: false,
-//       types: ["establishment"],
-//     }
-//   );
+var input1 = document.getElementById("from");
+var autocomplete1 = new google.maps.places.Autocomplete(input1, options);
+
+var input2 = document.getElementById("to");
+var autocomplete2 = new google.maps.places.Autocomplete(input2, options);
+
+// Get current Location
+const currentLocation = document.querySelector(".getCurrentLocation");
+currentLocation.addEventListener("click", () => {
+  navigator.geolocation.getCurrentPosition((position) => {
+    mapOptions.center.lat = position.coords.latitude;
+    mapOptions.center.lng = position.coords.longitude;
+
+    map = new google.maps.Map(document.getElementById("googleMap"), mapOptions);
+
+    fetch(
+      `https://maps.googleapis.com/maps/api/geocode/json?latlng=${position.coords.latitude},${position.coords.longitude}&key=AIzaSyAolXVBph__8LXk-JukgnxDUI4LPDQAsxQ`
+    )
+      .then((response) => response.json())
+      .then(
+        (response) => (input1.value = response.results[1].formatted_address)
+      );
+  });
+  map = new google.maps.Map(document.getElementById("googleMap"), mapOptions);
+});
+
+// // current position
+
+// function success(pos) {
+//   const crd = pos.coords;
+
+//   console.log("Your current position is:");
+//   console.log(`Latitude : ${crd.latitude}`);
+//   console.log(`Longitude: ${crd.longitude}`);
+//   console.log(`More or less ${crd.accuracy} meters.`);
 // }
 
-window.initMap = initMap;
+// function error(err) {
+//   console.warn(`ERROR(${err.code}): ${err.message}`);
+// }
 
-/*
-function initAutocomplete() {
-  const map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -33.8688, lng: 151.2195 },
-    zoom: 13,
-    mapTypeId: "roadmap",
-  });
-
-  const input = document.getElementById("pac-input");
-  const searchBox = new google.maps.places.SearchBox(input);
-
-  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-
-  // Bias the SearchBox results towards current map's viewport.
-  map.addListener("bounds_changed", () => {
-    searchBox.setBounds(map.getBounds());
-  });
-
-  let markers = [];
-
-  // Listen for the event fired when the user selects a prediction and retrieve
-  // more details for that place.
-  searchBox.addListener("places_changed", () => {
-    const places = searchBox.getPlaces();
-
-    if (places.length == 0) {
-      return;
-    }
-
-    // Clear out the old markers.
-    markers.forEach((marker) => {
-      marker.setMap(null);
-    });
-    markers = [];
-
-    // For each place, get the icon, name and location.
-    const bounds = new google.maps.LatLngBounds();
-
-    places.forEach((place) => {
-      if (!place.geometry || !place.geometry.location) {
-        console.log("Returned place contains no geometry");
-        return;
-      }
-
-      const icon = {
-        url: place.icon,
-        size: new google.maps.Size(71, 71),
-        origin: new google.maps.Point(0, 0),
-        anchor: new google.maps.Point(17, 34),
-        scaledSize: new google.maps.Size(25, 25),
-      };
-      markers.push(
-        new google.maps.Marker({
-          map,
-          icon,
-          title: place.name,
-          position: place.geometry.location,
-        })
-      );
-
-      if (place.geometry.viewport) {
-        // Only geocodes have viewport.
-        bounds.union(place.geometry.viewport);
-      } else {
-        bounds.extend(place.geometry.location);
-      }
-    });
-    map.fitBounds(bounds);
-  });
-}
-*/
+// navigator.geolocation.getCurrentPosition(success, error, options);

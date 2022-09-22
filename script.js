1ea37f4a750e53d79aab96a581e6c9808eecd66f
ShubhamVsCode <shubhamvscode@gmail.com>
2022-09-22 14:30:49 +0530

    Completed...

--- script.js
+++ script.js
@@ -2,32 +2,87 @@
 //set map options
 
 const kmMsg = document.querySelector(".km");
+let km;
 
-var myLatLng = { lat: 26.8467, lng: 80.9462 };
-var mapOptions = {
+let myLatLng = { lat: 26.8467, lng: 80.9462 };
+let mapOptions = {
   center: myLatLng,
   zoom: 12,
   mapTypeId: google.maps.MapTypeId.ROADMAP,
 };
 
 //create map
-var map = new google.maps.Map(document.getElementById("googleMap"), mapOptions);
+let map = new google.maps.Map(document.getElementById("googleMap"), mapOptions);
 
 //create a DirectionsService object to use the route method and get a result for our request
-var directionsService = new google.maps.DirectionsService();
+let directionsService = new google.maps.DirectionsService();
 
 //create a DirectionsRenderer object which we will use to display the route
-var directionsDisplay = new google.maps.DirectionsRenderer();
+let directionsDisplay = new google.maps.DirectionsRenderer();
 
 //bind the DirectionsRenderer to the map
 directionsDisplay.setMap(map);
+
+// selected vehical
+let selectedClass;
+const allVehical = document.querySelectorAll(".vehical-bar>div");
+allVehical.forEach((e) => {
+  e.addEventListener("click", (e) => {
+    selectedClass = e.path[1].classList;
+    selectedClass.forEach(
+      (c) =>
+        (c === "tucsonCar" || c === "dzireCar" || c === "mercedesCar") &&
+        priceCalculate(c)
+    );
+  });
+});
+
+const priceByCar = [
+  {
+    name: "tucsonCar",
+    displayName: "Tucson",
+    baseprice: 60,
+    perkm: 30,
+    img: `<img src="./assets/tucson.png" alt="">`,
+  },
+  {
+    name: "dzireCar",
+    displayName: "Dzire",
+    baseprice: 50,
+    perkm: 25,
+    img: `<img src="./assets/dzire.png" alt="">`,
+  },
+  {
+    name: "mercedesCar",
+    displayName: "Mercedes",
+    baseprice: 100,
+    perkm: 60,
+    img: `<img src="./assets/mercedes.png" alt="">`,
+  },
+];
+// price calculate for any car
+function priceCalculate(car) {
+  const selectedCar = priceByCar.find((carObject) => {
+    return carObject.name === car;
+  });
+
+  document.querySelector(".car").innerHTML = "by " + selectedCar.displayName;
+
+  const calculatedFare =
+    Math.floor(km * selectedCar.perkm) + selectedCar.baseprice;
+
+  const farePriceSpan = document.querySelector(".fare-price-span");
+  farePriceSpan.innerHTML = calculatedFare;
+
+  document.querySelector(".selected-car").innerHTML = selectedCar.img;
+}
 
 //define calcRoute function
 function calcRoute() {
   directionsDisplay.setMap(map);
 
   //create request
-  var request = {
+  let request = {
     origin: document.getElementById("from").value,
     destination: document.getElementById("to").value,
     travelMode: google.maps.TravelMode.DRIVING, //WALKING, BYCYCLING, TRANSIT
@@ -37,21 +92,42 @@ function calcRoute() {
   //pass the request to the route method
   directionsService.route(request, function (result, status) {
     if (status == google.maps.DirectionsStatus.OK) {
-      //Get distance and time
-      const output = document.querySelector("#output");
-      output.innerHTML =
-        "<div> The distance between " +
-        document.getElementById("from").value.bold() +
-        " & " +
-        document.getElementById("to").value.bold() +
-        " is " +
-        result.routes[0].legs[0].distance.text.bold() +
-        "." +
-        "<br />Duration  : " +
-        result.routes[0].legs[0].duration.text.bold() +
-        ".</div>";
-      const manResult = result.routes[0].legs[0].distance.text;
-      kmMsg.innerHTML = manResult;
+      // my--------------
+      const detailsContainer = document.querySelector(".details-container");
+      const fareContainer = document.querySelector(".fare-container");
+
+      detailsContainer.classList.remove("hidden");
+      fareContainer.classList.remove("hidden");
+
+      km = result.routes[0].legs[0].distance.value / 1000;
+
+      // calculate fare
+      const calculatedFare =
+        Math.floor((result.routes[0].legs[0].distance.value / 1000) * 30) + 60;
+
+      const farePriceSpan = document.querySelector(".fare-price-span");
+      farePriceSpan.innerHTML = calculatedFare;
+
+      detailsContainer.innerHTML = `<div class="distance-container">
+          <p>Distance</p>
+          <div class="distance-from">from : <br /> <span class="distance-from-span">${
+            document.getElementById("from").value
+          }</span></div>
+          <div class="distance-to">to : <br /> <span class="distance-to-span">${
+            document.getElementById("to").value
+          }</span></div>
+
+          <div class="distance">${result.routes[0].legs[0].distance.text}</div>
+      </div>
+
+      <div class="separator"></div>
+
+      <div class="duration-container">
+          <p>Duration</p>
+          <div class="vehical">by <span class="vehical-span">Sedan</span></div>
+
+          <div class="duration">${result.routes[0].legs[0].duration.text}</div>
+      </div>`;
 
       //display route
       directionsDisplay.setDirections(result);
@@ -69,15 +145,15 @@ function calcRoute() {
 }
 
 //create autocomplete objects for all inputs
-var options = {
+let options = {
   types: ["(cities)"],
 };
 
-var input1 = document.getElementById("from");
-var autocomplete1 = new google.maps.places.Autocomplete(input1, options);
+let input1 = document.getElementById("from");
+let autocomplete1 = new google.maps.places.Autocomplete(input1, options);
 
-var input2 = document.getElementById("to");
-var autocomplete2 = new google.maps.places.Autocomplete(input2, options);
+let input2 = document.getElementById("to");
+let autocomplete2 = new google.maps.places.Autocomplete(input2, options);
 
 // Get current Location
 const currentLocation = document.querySelector(".getCurrentLocation");
@@ -98,20 +174,3 @@ currentLocation.addEventListener("click", () => {
   });
   map = new google.maps.Map(document.getElementById("googleMap"), mapOptions);
 });
-
-// // current position
-
-// function success(pos) {
-//   const crd = pos.coords;
-
-//   console.log("Your current position is:");
-//   console.log(`Latitude : ${crd.latitude}`);
-//   console.log(`Longitude: ${crd.longitude}`);
-//   console.log(`More or less ${crd.accuracy} meters.`);
-// }
-
-// function error(err) {
-//   console.warn(`ERROR(${err.code}): ${err.message}`);
-// }
-
-// navigator.geolocation.getCurrentPosition(success, error, options);
